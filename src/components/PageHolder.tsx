037a6df477eba7c63779ef60dbdc9e3a215ac978
Prajwal Mandlik <prajwalmandlik2004@gmail.com>
2025-01-11 01:17:09 +0530

    header section update

--- src/components/PageHolder.tsx
+++ src/components/PageHolder.tsx
@@ -1,13 +1,13 @@
 function PageHolder() {
     return (
       <div className="flex lg:pl-14 ml-4 pt-4">
-        <div className="text-sm text-[#3E5765]">
+        <div className="text-sm mr-2 text-[#3E5765]">
           Cryptocurrencies
         </div>
         <div className="text-sm text-[#3E5765]">
-          {' >> '} 
+          {' » '} 
         </div>
-        <div className="text-sm text-[#0F1629]">
+        <div className="text-sm ml-2 text-[#0F1629]">
           Bitcoin
         </div>
       </div>
